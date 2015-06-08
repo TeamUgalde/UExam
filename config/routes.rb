2fc43a1351e823b6713ce5b83848e5250a9cc0b2
diugalde <dua.0695@gmail.com>
2015-06-08 16:59:32 -0600

    User authentication finished
    - Alerts working.
    - Facebook authentication working.
    - Register and Logout working.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", :sessions => "users/sessions", :registrations => "users/registrations"}
 end
